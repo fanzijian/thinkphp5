c21a428491e18e7ed94756316727542b2d7e32ce
omg_two <1158534904@qq.com>
2016-12-09 16:13:54 +0800

    添加静态页面于user下

--- application/index/controller/UserController.php
+++ application/index/controller/UserController.php
@@ -13,13 +13,63 @@ class UserController extends Controller
 	public function index()
 	{
 
-		$htmls = $this->fetch('lessonAnalysis');
+		$htmls = $this->fetch('lessonList');
 		return $htmls;
 	}
 
 	public function showLessonDetail()
 	{
 		return $this->fetch('lessonDetail');
+	}
+
+	public function showLessonAnalysisList()
+	{
+		return $this->fetch('lessonAnalysisList');
+	}
+
+	public function showLessonAnalysisDetail()
+	{
+		return $this->fetch('lessonAnalysisDetail');
+	}
+
+	public function showPaperList()
+	{
+		return $this->fetch('paperList');
+	}
+
+	public function showPaperProcessDetail()
+	{
+		return $this->fetch('paperProcessDetail');
+	}
+
+	public function showPaperQuestionAnalysisList()
+	{
+		return $this->fetch('paperQuestionAnalysisList');
+	}
+
+	public function showPaperQuestionAnalysisDetail()
+	{
+		return $this->fetch('paperQuestionAnalysisDetail');
+	}
+
+	public function showSelfLearningList()
+	{
+		return $this->fetch('selfLearningList');
+	}
+
+	public function showSelfLearningDetail()
+	{
+		return $this->fetch('selfLearningDetail');
+	}
+
+	public function showSelfLearningAnalysisList()
+	{
+		return $this->fetch('selfLearningAnalysisList');
+	}
+
+	public function showSelfLearningAnalysisDetail()
+	{
+		return $this->fetch('selfLearningAnalysisDetail');
 	}
 }
 
